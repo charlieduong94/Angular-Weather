f7d2afad8b4699983ce6956f98448a98571fae15
Charlie Duong <charlieduong94@gmail.com>
2016-01-09 18:20:40 -0500

    Update ForecastFactory.js

--- app/components/WeatherForecast/services/ForecastFactory.js
+++ app/components/WeatherForecast/services/ForecastFactory.js
@@ -3,7 +3,7 @@ angular.module('Forecast').factory("WeatherFactory", function($http){
   return {
     getWeather : function(service, lat, lon){
       return $http({
-        url: "http://charlie-duong.com/weatherApp", 
+        url: "http://charlie-duong.com/weather/forecast", 
         method: "GET",
         params: {lat : lat, lon : lon}
      });
